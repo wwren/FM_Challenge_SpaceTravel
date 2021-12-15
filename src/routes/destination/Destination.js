afbcbd7a3a10bfdad04cd197ba4dff9ef6f67952
wwren <rdding@student.unimelb.edu.au>
2021-12-15 19:40:32 +1100

    Added control panel to destination

--- src/routes/destination/Destination.js
+++ src/routes/destination/Destination.js
@@ -1,40 +1,55 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import MainWrapper from "../../commonComponents/MainWrapper";
-import { pageTopLeftIntro, distanceBottomTitle } from "../../space-data";
+import { pageTopLeftIntro, distanceBottomTitle, getDestination } from "../../space-data";
 import "./Destination.css";
 
-const exp = {
-  name: "Moon",
-  images: {
-    png: "/destination/image-moon.png",
-    webp: "/destination/image-moon.webp",
-  },
-  description:
-    "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
-  distance: "384,400 km",
-  travel: "3 days",
-};
-
 function Destination() {
+  const destination = getDestination();
+  const [focusContent, setFocusContent] = useState(destination[0]);
+  const controlRef = useRef(null);
+
+  const _handleClick = (e, idx) => {
+    setFocusContent(destination[idx]);
+
+    // remove .avtive to classList
+    if (controlRef) {
+      controlRef.current.childNodes.forEach((node, i) => {
+        if (node.classList.contains("active") && idx !== i) {
+          node.classList.remove("active");
+        }
+      });
+    }
+    e.target.classList.add("active");
+  };
+
   return (
     <main className="destination">
       <MainWrapper text={pageTopLeftIntro.destinations}>
         <img
           className="destination__left-panel"
-          src={require(`../../assets${exp.images.png}`).default}
-          alt={exp.name}
+          src={require(`../../assets${focusContent.images.png}`).default}
+          alt={focusContent.name}
         />
         <div className="destination__right-panel">
-          <h3 className="text--highlight text--highlight--destination">{exp.name}</h3>
-          <p className="text--last-p"> {exp.description}</p>
+          <div className="control-panel" ref={controlRef}>
+            {destination.map((ele, idx) => {
+              return (
+                <button key={ele.name} onClick={(e) => _handleClick(e, idx)} className={`${idx == 0 ? "active" : ""}`}>
+                  {ele.name}
+                </button>
+              );
+            })}
+          </div>
+          <h3 className="text--highlight text--highlight--destination">{focusContent.name}</h3>
+          <p className="text--last-p"> {focusContent.description}</p>
           <div className="destination__bottom-panel">
             <div>
               <h4>{distanceBottomTitle.title1}</h4>
-              <p>{exp.distance}</p>
+              <p>{focusContent.distance}</p>
             </div>
             <div>
               <h4>{distanceBottomTitle.title2}</h4>
-              <p>{exp.travel}</p>
+              <p>{focusContent.travel}</p>
             </div>
           </div>
         </div>
